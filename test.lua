d8fe3738c773ea2d8569682c6d305c0b786cbc7e
Emil Christiansen <emilsen68@gmail.com>
2021-02-15 22:09:19 +0100

    Color support and new data structure
    Changed a few small things, and one big thing. The way the LUA code processes the data sent to it is now done with less code, and the code is made more readable. The ability to change the color of what you are drawing on the screen is now possible.
    Also, drawType has been renamed to mode, since it does much more than drawing now. The reason for changing the way that data is processed can be seen in the way data is sent. Data can be sent with strings as indexes, and the frontend in Stormworks will turn the data into an array of all the commands sent from the file. This data will be rendered as one image.

--- test.lua
+++ test.lua
@@ -1,29 +1,24 @@
 commandTable = {}
 commands = {}
-data = "drawType=line, startX=5, startY=5, endX=20, endY=20\ndrawType=line, startX=10, startY=10, endX=20, endY=20"
+data = "mode=line, startX=5, startY=5, endX=20, endY=20\nmode=line, startX=10, startY=10, endX=20, endY=20"
 
 i = 1
 for command in data:gmatch("([^\n]*)\n?") do
     commands = {}
     for k, v in string.gmatch(command, "(%w+)=(%w+)") do
-        if k == "drawType" then
-            commands[1] = v
-        elseif k == "startX" then
-            commands[2] = v
-        elseif k == "startY" then
-            commands[3] = v
-        elseif k == "endX" then
-            commands[4] = v
-        elseif k == "endY" then
-            commands[5] = v
-        end
+        commands[k] = v
+        print(k, v)
     end
     commandTable[i] = commands
     i = i+1
 end
+print("\n")
 
-for k, v in  pairs(commandTable) do
-    for k, v in pairs(v) do
-        print(v)
-    end
-end
+print(commandTable[1]["startX"])
+
+-- for k, v in  pairs(commandTable) do
+--     for k, v in pairs(v) do
+--         print("\n")
+--         print(v)
+--     end
+-- end
